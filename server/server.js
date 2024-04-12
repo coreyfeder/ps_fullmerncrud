69b23959443e797f6fffdcdcbd36520ee23782a5
Corey Feder <corey.feder@gmail.com>
2024-04-12 12:38:16 -0400

    PUT and POST updates now working properly

--- server/server.js
+++ server/server.js
@@ -23,82 +23,107 @@ app.get("/", (req, res) => {
 
 // read all
 app.get("/notes", async (req, res) => {
+    console.debug("DEBUG: notes > read all")
     const notes = await Note.find();
-    console.debug(`Notes found: ${notes.length}`)
+    // console.debug(`Notes found: ${notes.length}`)
     res.json({notes: notes})
 });
 
 // read one
 app.get("/notes/:note_id", async (req, res) => {
+    console.debug("DEBUG: notes > read one > ", req.params.note_id)
     const note_id = req.params.note_id;
-    const notes = await Note.findById(note_id);
+    const notes = await Note.findById(note_id);  // easy way
+    // const notes = await Note.find({ _id: note_id });  // hard way
     // console.debug(`Note ${note_id} result: ${notes}`)
     if (!(notes)) {
         console.debug(`Requested note_id "${note_id}" did not match any records.`)
-    } else {
-        console.debug(`Note ${note_id} found.`)
+    // } else {
+        // console.debug(`Note ${note_id} found.`)
     }
     res.json({notes: notes})
 });
 
 // create
 app.post("/notes", async (req, res) => {
-    // get body request
-    const title = req.body.title;
-    const body = req.body.body;
-    // res.json({post: "note", title: title, body: body})
+    console.debug("DEBUG: notes > create")
+    // get note from body request
+    // const title = req.body.title;
+    // const body = req.body.body;
     // create note
     const note = await Note.create({
-        title: title,
-        body: body,
+        // any benefit to using the indermediary variables other than debugging?
+        // title: title,
+        // body: body,
+        title: req.body.title,
+        body: req.body.body,
     });
     // respond with new note
-    console.debug(`Note ${note._id} created.`)
+    console.debug(`Note ${note._id} created:  `, note)
     res.json({note: note})
 });
 
-// update (replace)
+
+// update (replace) (no upsert)
 app.post("/notes/:note_id", async (req, res) => {
-    console.debug("DEBUG: U-post: Note ID: ", req.params.note_id)
-    console.debug("DEBUG: U-post: New body: ", req.body)
+    console.debug("DEBUG: notes > update (replace) > ", req.params.note_id)
+    console.debug("DEBUG: new body: ", req.body)
     const note_id = req.params.note_id;
     let notes;
     notes = await Note.findById(note_id);
-    console.debug("DEBUG: Found? ", notes)
+    // console.debug("DEBUG: former value: ", await Note.findById(note_id))
+    console.debug("DEBUG: former value: ", notes)
     if (!(notes)) {
-        console.debug(`DEBUG: Requested note_id "${note_id}" did not match any records.`)
+        console.log(`DEBUG: Requested note_id "${note_id}" did not match any records.`)
     } else {
         console.debug(`DEBUG: Note_id "${note_id}" found: `, notes)
-        console.debug("DEBUG: New body: ", req.body)
-        // notes = await Note.findOneAndReplace( { _id: note_id }, req.body )
+        notes = await Note.findOneAndReplace(
+            { _id: note_id },
+            req.body,
+            options={new: true, lean: true}
+        )
+    }
+    console.debug("DEBUG: new value: ", notes)
+    res.json({notes: notes})
+});
+
+
+// update (by field) (no upsert)
+app.put("/notes/:note_id", async (req, res) => {
+    console.debug("DEBUG: notes > update (by field) > ", req.params.note_id)
+    console.debug("DEBUG: new body: ", req.body)
+    const note_id = req.params.note_id;
+    let notes;
+    notes = await Note.findById(note_id);
+    if (!(notes)) {
+        console.log(`DEBUG: Requested note_id "${note_id}" did not match any records.`)
+    } else {
+        // console.debug(`DEBUG: Note_id "${note_id}" found: `, notes)
         notes = await Note.findByIdAndUpdate(
-            note_id, req.body, options={returnDocument: 'after', new: true, upsert: false}
+            note_id, req.body, options={new: true, upsert: false, lean: true, remove: false}
         )
     }
     res.json({notes: notes})
 });
 
-// update (by field)
-app.put("/notes/:note_id", async (req, res) => {
-    console.debug("DEBUG: U-put: Note ID: ", req.params.note_id)
-    console.debug("DEBUG: U-put: New body: ", req.body)
+// delete one
+app.delete("/notes/:note_id", async (req, res) => {
+    console.debug("DEBUG: notes > delete one > ", req.params.note_id)
     const note_id = req.params.note_id;
-    let notes;
-    notes = await Note.findById(note_id);
-    console.debug("DEBUG: Found? ", notes)
-    if (!(notes)) {
-        console.debug(`DEBUG: Requested note_id "${note_id}" did not match any records.`)
-    } else {
-        console.debug(`DEBUG: Note_id "${note_id}" found: `, notes)
-        console.debug("DEBUG: New body: ", req.body)
-        // notes = await Note.findOneAndReplace( { _id: note_id }, req.body )
-        // notes = await Note.updateOne(
-        notes = await Note.findByIdAndUpdate(
-            note_id, req.body, options={returnDocument: 'after', new: true, upsert: false}
-        )
-    }
-    res.json({notes: notes})
+    const before = await Note.findById(note_id);
+    // const response = await Note.deleteOne({ _id: note_id });  // oh, you dummy
+    const response = await Note.findByIdAndDelete(note_id);
+    // console.debug(`Note ${note_id} result: ${notes}`)
+    // if (!(notes)) {
+    //     console.debug(`Requested note_id "${note_id}" did not match any records.`)
+    // } else {
+    //     console.debug(`Note ${note_id} found.`)
+    // }
+    const after = await Note.findById(note_id);
+    res.json({response: response, before: before, after: after})
 });
+
+
 
 // different axis
 // app.route("/tasks")
